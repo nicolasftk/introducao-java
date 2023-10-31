c945c5c4dd998099435f501f9965845104348488
Nicolas <nclkiler@hotmail.com>
2023-10-30 23:36:44 -0300

    primeiro commit

---
+++ src/introducao/TiposPrimitivos.java
@@ -0,0 +1,27 @@
+package introducao;
+
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+    //int, double, float, char, byte, short, long, boolean
+
+        int idade =5;
+        long numeroGrande = 50000;
+        double salarioDouble = 2000.3;
+        float salarioFloat = 2500.22F;
+        byte idadeByte = 10;
+        short idadeShort =10;
+        boolean verdade = true;
+        boolean falso = false;
+        char caractere = 125;
+
+        System.out.println("Eu tenho " + idade + " anos.");
+        System.out.println(caractere);
+        System.out.println(numeroGrande);
+        System.out.println(salarioDouble);
+        System.out.println(salarioFloat);
+
+
+    }
+
+
+}
